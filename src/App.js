2342d3fb2adc5843a548693c9ddb434a55944e89
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-04-07 15:06:07 +0530

    ik

--- src/App.js
+++ src/App.js
@@ -7,18 +7,27 @@ import Footer from "./components/footer";
 import Parent from "./propsPassing/Parent";
 import Child from "./propsPassing/Child";
 
-function App() {
-  return (
-    <>
-      {/* <Navbar />
-      <HeroSection />
-      <Features />
-      <Testimonials />
-      <Footer /> */}
-      <Parent/>
-      <Child/>
-    </>
-  );
-}
+// function App() {
+//   return (
+//     <>
+//       {/* <Navbar />
+//       <HeroSection />
+//       <Features />
+//       <Testimonials />
+//       <Footer /> */}
+//       <Parent/>
+//       <Child/>
+//     </>
+//   );
+// }
 
+
+function App() {
+  const isLoggedIn = false;
+  return(
+    <div>
+      {isLoggedIn ? <h1>Welcome User </h1> : <h1>Please Login </h1>}
+    </div>
+  )
+}
 export default App;
---
+++ src/ConceptComp/IfElseComp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const IfElseComp = () => {
+    const agent = false;
+  return (
+    <>
+    <div>IfElseComp</div>
+    <h2>{agent?"khatam tata goodbye gaya":"welcome to Canaadaaaaaa"}</h2>
+    </>
+  )
+}
+
+export default IfElseComp
